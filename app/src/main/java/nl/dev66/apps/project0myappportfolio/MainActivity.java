003a0ae3549326f7ab09c9300464142c06abf761
Edwin de Jongh <edwin@dev66.nl>
2015-09-20 10:41:40 +0200

    test commit added a space

--- app/src/main/java/nl/dev66/apps/project0myappportfolio/MainActivity.java
+++ app/src/main/java/nl/dev66/apps/project0myappportfolio/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-// main activity
+// main activity 
 public class MainActivity extends AppCompatActivity {
 
     @Override
